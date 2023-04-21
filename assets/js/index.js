08006471f145d3496b97903f375581bf84af90e2
Marcos <vinisantos.1395@gmail.com>
2023-04-21 17:21:03 -0300

    atualizacao

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,8 @@ function addLinkIframe(titulo, link) {
     $("#modalTransparency").modal("show");
     $("#modalTransparency iframe").attr("src", link);
     $("#modalTransparency #modalTransparencyLabel").html(titulo);
+}
 
+function closeModal() {
+    $("#modalTransparency").modal("hide");
 }
